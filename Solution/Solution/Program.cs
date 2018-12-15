f7ed339a4a00e85506ca24c3c10949a6be9e13bc
wangkai03 <wangkai03@HIH-D-16195>
2018-12-15 16:03:19 +0800

    6.ZigZag

--- Solution/Solution/Program.cs
+++ Solution/Solution/Program.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace Solution
 {
+    public class Solution
+    {
+        public string ZigZag(string s, int line)
+        {
+            string ret = string.Empty;
+            int len = s.Length;
+            List<char>[] help = new List<char>[line];
+            for (int i = 0; i < line; ++i)
+            {
+                help[i] = new List<char>();
+
+                for (int j = 0; j < len/line; ++j)
+                {
+                    int index = j*(2*line - 2) + i;
+                    if(index<len)
+                        help[i].Add(s[index]);
+                    if (i != 0 && i != line - 1)
+                    {
+                        int index2 = index + (line - i - 1)*2;
+                        if (index2 < len)
+                            help[i].Add(s[index2]);
+                    }
+                }
+                
+            }
+            foreach (var lst in help)
+            {
+                ret = lst.Aggregate(ret, (current, c) => current + c);
+            }
+            return ret;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Solution s = new Solution();
+            string ret = s.ZigZag("PAYPALISHIRING", 4);
+            Console.WriteLine(ret);
         }
     }
 }
