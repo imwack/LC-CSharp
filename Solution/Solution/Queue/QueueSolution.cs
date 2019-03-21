49f0c625e881bfcf9f0abfe2d203c97d1e177c7e
imwack <i.am.wack.1993@gmail.com>
2019-03-21 22:13:23 +0800

    346 Moving Average from Data Stream

--- Solution/Solution/Queue/QueueSolution.cs
+++ Solution/Solution/Queue/QueueSolution.cs
@@ -10,7 +10,7 @@ namespace Solution.Queue
     {
         #region queue
 
-        //933. Number of Recent Calls
+        // 933. Number of Recent Calls
         //Just like message queue , each time enque need check out of date item 
         public class RecentCounter
         {
@@ -29,6 +29,31 @@ namespace Solution.Queue
                 }
                 queue.Enqueue(t);
                 return queue.Count;
+            }
+        }
+
+
+        // 346	Moving Average from Data Stream
+        // Like slide window , with max window size, when window is full dequeue
+        public class MovingAverage
+        {
+            Queue<int> q = new Queue<int>();
+            public int Count;
+            public int Sum = 0;
+            MovingAverage(int size)
+            {
+                Count = size;
+            }
+
+            double Next(int val)
+            {
+                if (q.Count >= Count)
+                {
+                    Sum -= q.Dequeue();
+                }
+                q.Enqueue(val);
+                Sum += val;
+                return Sum*1.0/q.Count;
             }
         }
 
