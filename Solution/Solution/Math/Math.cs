49e415ed3302cd704de8aaae2730df4fcb547f0b
imwack <i.am.wack.1993@gmail.com>
2019-03-23 15:27:53 +0800

    908. Smallest Range I

---
+++ Solution/Solution/Math/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    public partial class MySolution
+    {
+        // 908. Smallest Range I
+        // just need to find the range of min and max
+        // eg. [min-k, min+k] [max-k, max+k]
+        // if the two Section intersect then each num can change to the intersection, or just the max gap
+        public int SmallestRangeI(int[] A, int K)
+        {
+            int total = 0;
+            int l = 0, r = A.Length - 1;
+            Array.Sort(A); // only need the max and min can be O(n)
+
+            if (A[l] + K >= A[r] - K)
+            {
+                return total;
+            }
+            else
+            {
+                return A[r] - K - A[l] - K;
+            }
+        }
+    }
+}
---
+++ Solution/Solution/Tree/Tree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    public partial class MySolution
+    {
+        //559. Maximum Depth of N-ary Tree
+        public class NodeN
+        {
+            public int val;
+            public IList<NodeN> children;
+
+            public NodeN() { }
+            public NodeN(int _val, IList<NodeN> _children)
+            {
+                val = _val;
+                children = _children;
+            }
+            public int GetDepth(NodeN root, int h)
+        {
+            if (root == null)
+            {
+                return h;
+            }
+            int maxh = h;
+            foreach (var r in root.children)
+            {
+                maxh = Math.Max(maxh, GetDepth(r, h + 1));
+            }
+            return maxh;
+        }
+        public int MaxDepth(NodeN root)
+        {
+            if (root == null)
+                return 0;
+            return GetDepth(root, 1);
+        }
+    }
+
+}
