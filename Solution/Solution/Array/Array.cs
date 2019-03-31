0c36074aca3516e94650eecb38442a43e14f83cc
imwack <i.am.wack.1993@gmail.com>
2019-03-31 16:15:16 +0800

    1013. Pairs of Songs With Total Durations Divisible by 60

--- Solution/Solution/Array/Array.cs
+++ Solution/Solution/Array/Array.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +24,36 @@ namespace Solution.Array
                     l = mid + 1;
             }
             return -1;
+        }
+
+        public IList<bool> PrefixesDivBy5(int[] A)
+        {
+            IList<bool> ret = new List<bool>(A.Length);
+            int num = 0;
+            foreach (int i in A)
+            {
+                num = num*2 + i;
+                if(num%5==0)
+                    ret.Add(true);
+                else
+                    ret.Add(false);
+            }
+            return ret;
+        }
+        public int NumPairsDivisibleBy60(int[] time)
+        {
+            int total = 0;
+            int[] cnt = new int[60];
+            for (int i = 0; i < time.Length; i++)
+            {
+                int remain = time[i] % 60;
+                if (remain == 0)
+                    total += cnt[0];
+                else
+                    total += cnt[60 - remain];
+                ++cnt[remain];
+            }
+            return total;
         }
     }
 }
