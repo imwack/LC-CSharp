f7c725ee8a617330d47117fdaeb57cc465a0edf1
imwack <i.am.wack.1993@gmail.com>
2019-04-03 21:00:38 +0800

    13. Roman to Integer

--- Solution/Solution/MS/MS.cs
+++ Solution/Solution/MS/MS.cs
@@ -46,6 +46,40 @@ namespace Solution
             return (int)value;
         }
 
-
+        //[13] Roman to Integer
+        /*    I             1
+            * V             5
+            * X             10
+            * L             50
+            * C             100
+            * D             500
+            * M             1000
+        */
+        public int RomanToInt(string s)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>
+            {
+                ['I'] = 1,
+                ['V'] = 5,
+                ['X'] = 10,
+                ['L'] = 50,
+                ['C'] = 100,
+                ['D'] = 500,
+                ['M'] = 1000
+            };
+            int n = 0;
+            for (int i = 0;i<s.Length;i++)
+            {
+                if (i == s.Length - 1 || dic[s[i]] >= dic[s[i + 1]])
+                {
+                    n += dic[s[i]];
+                }
+                else
+                {
+                    n -= dic[s[i]];
+                }
+            }
+            return n;
+        }
     }
 }
