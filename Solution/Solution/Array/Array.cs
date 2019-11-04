bb4a3e8fdeca9761fb05e393d54c17c68741e5af
imwack <i.am.wack.1993@gmail.com>
2019-11-04 22:48:23 +0800

    81 搜索旋转排序数组 II

--- Solution/Solution/Array/Array.cs
+++ Solution/Solution/Array/Array.cs
@@ -10,6 +10,31 @@ namespace Solution
 {
     public partial class MySolution
     {
+        public bool Search2(int[] nums, int target)
+        {
+            int l = 0, r = nums.Length - 1;
+            while (l<r)
+            {
+                int mid = (r + l)/2;
+                if (nums[mid] == target) return true;
+                if (nums[l] <= nums[mid]) //左边有序
+                {
+                    if (nums[l] <= target && nums[mid] > target)
+                        r = mid - 1;
+                    else
+                        l = mid + 1;
+                }
+                else //右边有序
+                {
+                    if (nums[r] >= target && nums[mid] < target)
+                        l = mid + 1;
+                    else
+                        r = mid - 1;
+                }
+            }
+            return false;
+        }
+
         public IList<int> MajorityElement(int[] nums)
         {
             IList<int> res = new List<int>();
