5acd7f3a993400d76c1b9c40e634cdf235d85185
imwack <i.am.wack.1993@gmail.com>
2019-11-10 00:42:54 +0800

    NumMatrix

--- Solution/Solution/DP/DP.cs
+++ Solution/Solution/DP/DP.cs
@@ -8,6 +8,7 @@ namespace Solution
 {
     public partial class MySolution
     {
+
         public bool CheckSubarraySum(int[] nums, int k)
         {
             int sum = 0;
@@ -245,5 +246,33 @@ namespace Solution
         }
 
 
+    }
+    public class NumMatrix
+    {
+        public int[,] dp;
+        public NumMatrix(int[][] matrix)
+        {
+            if (matrix.Length == 0)
+            {
+                dp = new int[0, 0];
+                return;
+            }
+            dp = new int[matrix.Length + 1, matrix[0].Length + 1];
+
+            for (int i = 1; i <= matrix.Length; i++)
+            {
+                for (int j = 1; j <= matrix[0].Length; j++)
+                {
+                    dp[i, j] = dp[i - 1, j] + dp[i, j - 1] - dp[i - 1, j - 1] + matrix[i - 1][j - 1];
+                }
+            }
+        }
+
+        public int SumRegion(int row1, int col1, int row2, int col2)
+        {
+            if (dp.Length == 0)
+                return 0;
+            return dp[row2 + 1, col2 + 1] - dp[row2 + 1, col1] - dp[row1, col2 + 1] + dp[row1, col1];
+        }
     }
 }
