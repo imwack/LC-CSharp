c16d0959a17e9bb1e84541a4382263cecbd8d945
imwack <i.am.wack.1993@gmail.com>
2019-11-16 17:50:49 +0800

    MyCircularQueue

--- Solution/Solution/Design/Design.cs
+++ Solution/Solution/Design/Design.cs
@@ -6,6 +6,74 @@ using System.Threading.Tasks;
 
 namespace Solution.Design
 {
+    public class MyCircularQueue
+    {
+        private int[] elements;
+        private int first = 0;
+        private int last = 0;
+        /** Initialize your data structure here. Set the size of the queue to be k. */
+        public MyCircularQueue(int k)
+        {
+            elements = new int[k+1];
+        }
+
+        /** Insert an element into the circular queue. Return true if the operation is successful. */
+        public bool EnQueue(int value)
+        {
+            if (IsFull())
+            {
+                return false;
+            }
+            if (IsEmpty())
+                elements[first] = value;
+            else
+                elements[last] = value;
+            last++;
+            if (last == elements.Length) last = 0;
+            return true;
+        }
+
+        /** Delete an element from the circular queue. Return true if the operation is successful. */
+        public bool DeQueue()
+        {
+            if (IsEmpty()) return false;
+            first++;
+            if (first == elements.Length) first = 0;
+            return true;
+        }
+
+        /** Get the front item from the queue. */
+        public int Front()
+        {
+            if (IsEmpty()) return -1;
+            return elements[first];
+        }
+
+        /** Get the last item from the queue. */
+        public int Rear()
+        {
+            if (IsEmpty()) return -1;
+
+            if (last == 0) return elements[elements.Length - 1];
+            return elements[last - 1];
+        }
+
+        /** Checks whether the circular queue is empty or not. */
+        public bool IsEmpty()
+        {
+            return last == first;
+        }
+
+        /** Checks whether the circular queue is full or not. */
+        public bool IsFull()
+        {
+            if(first == last) return false;
+ 
+            return (last + 1)%elements.Length == first;
+        }
+    }
+
+
     public class MyLinkedList
     {
         DoubleListNode head = new DoubleListNode(0);
