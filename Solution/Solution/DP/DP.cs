23ea445d1f2a8572f2e00c99d32f07a9a7eb355f
imwack <i.am.wack.1993@gmail.com>
2019-11-19 19:29:16 +0800

    MinEditDistance

--- Solution/Solution/DP/DP.cs
+++ Solution/Solution/DP/DP.cs
@@ -8,6 +8,33 @@ namespace Solution
 {
     public partial class MySolution
     {
+        public int MinEditDistance(string word1, string word2)
+        {
+            int[,] dp = new int[word1.Length+1,word2.Length+1];
+            for (int i = 1; i <= word2.Length; i++)
+            {
+                dp[0, i] = i;
+            }
+            for (int i = 1; i <= word1.Length; i++)
+            {
+                dp[i, 0] = i;
+            }
+            for (int i = 1; i <= word1.Length; i++)
+            {
+                for (int j = 1; j <= word2.Length; j++)
+                {
+                    dp[i, j] = Math.Min(dp[i - 1, j], dp[i, j - 1]) + 1;
+
+                    if (word1[i - 1] == word2[j - 1])
+                        dp[i, j] = Math.Min(dp[i, j], dp[i - 1, j - 1] );
+                    else
+                        dp[i, j] = Math.Min(dp[i, j], dp[i - 1, j - 1] + 1);
+                }
+            }
+
+            return dp[word1.Length, word2.Length];
+        }
+
         public int MaxSumAfterPartitioning(int[] A, int K)
         {
             int[] dp = new int[A.Length+1];
