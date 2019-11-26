4909368975803c98e8514fb8a91f86dbebda4ce0
imwack <i.am.wack.1993@gmail.com>
2019-11-26 17:19:05 +0800

    FindMin

--- Solution/Solution/BinarySearch/BinarySearch.cs
+++ Solution/Solution/BinarySearch/BinarySearch.cs
@@ -8,6 +8,30 @@ namespace Solution
 {
     public partial class MySolution
     {
+        public int FindMin(int[] nums)
+        {
+            int l = 0, r = nums.Length - 1;
+            while (l<=r)
+            {
+                int mid = (l + r) / 2;
+                if (nums[mid] > nums[l])
+                {
+                    if (nums[l] > nums[r])
+                        l = mid + 1;
+                    else
+                        r = mid - 1;
+                }
+                else
+                {
+                    if (nums[mid] < nums[r])
+                        r = mid - 1;
+                    else
+                        l = mid + 1;
+                }
+            }
+            return nums[l]>nums[r]?nums[r]:nums[l];
+        }
+
         public int HIndex(int[] citations)
         {
             int n = citations.Length;
